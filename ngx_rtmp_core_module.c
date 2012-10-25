2be1fb0a08ff0b3b21ad0aa240a9a3602eecc8f4
Toda Takahiko <poepoemix@hotmail.com>
2012-10-25 22:14:37 +0900

    テストでいれたログコメントをコメントアウトした。

--- ngx_rtmp_core_module.c
+++ ngx_rtmp_core_module.c
@@ -492,8 +492,8 @@ ngx_rtmp_core_listen(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 #endif
 
     // TODO 調子にのってこっちにもいれてみる。
-    ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
-                       "this is the log testtest...");
+//    ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+//                       "this is the log testtest...");
 
     value = cf->args->elts;
 
